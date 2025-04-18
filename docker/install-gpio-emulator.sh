c9d7756d560af93fb2d6b29f51d86b2ae7f69f45
Dolphin <112799379+dongorae@users.noreply.github.com>
2025-04-18 15:23:56 +0900

    dd

--- docker/install-gpio-emulator.sh
+++ docker/install-gpio-emulator.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt install -y python3-pip libgpiod2 libgpiod-dev fuse3
+sudo apt update
+sudo apt install -y python3-pip libgpiod2 libgpiod-dev fuse3
+sudo python3 -m pip config set global.break-system-packages true
 sudo pip3 install gpiod-sysfs-proxy
