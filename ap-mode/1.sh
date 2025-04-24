32b1769cea71ff7b9323a2ca4ee67d4b6d6c7483
Dolphin <112799379+dongorae@users.noreply.github.com>
2025-04-24 13:34:58 +0900

    ㅇㅇ

--- ap-mode/1.sh
+++ ap-mode/1.sh
@@ -6,10 +6,10 @@ sudo tee /etc/hostapd/hostapd.conf > /dev/null <<'EOF'
 interface=ap0
 ssid=my_ssid_name
 hw_mode=g
-channe1=6
+channel=6
 ieee80211n=1
 wmm_enabled=1
-macaddr_ac1=0
+macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
 wpa=2
