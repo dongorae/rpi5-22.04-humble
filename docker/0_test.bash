6d33e1ec1d81cdcee0252e7eca0d334ae20afc78
Dolphin <112799379+dongorae@users.noreply.github.com>
2025-06-30 15:57:07 +0900

    Update 0_test.bash

--- docker/0_test.bash
+++ docker/0_test.bash
@@ -1,2 +1,16 @@
 wget https://desktop.docker.com/linux/main/arm64/187762/docker-desktop-arm64.deb
 sudo apt install ./docker-desktop-arm64.deb
+
+docker run -d \
+  --name deepracer \
+  --privileged \
+  --network host \
+  --cgroupns=host \
+  -v /sys:/sys \
+  -v /dev:/dev \
+  -v /opt:/opt \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /home/deepracer/Desktop/workspace:/workspace \
+  -e DISPLAY=$DISPLAY \
+  deepracer:latest \
+  bash
